2ea5c4621573f2c8a45919e4d26b16fca0dc36a5
minsmiths <minggi1216@gmail.com>
2026-01-29 02:56:58 +0900

    중간완성

--- Deargrandmama/Assets/Scenes/Minseo_Scenes/RecipeBook/RecipeManager.cs
+++ Deargrandmama/Assets/Scenes/Minseo_Scenes/RecipeBook/RecipeManager.cs
@@ -36,7 +36,7 @@ public class RecipeManager : MonoBehaviour
 
             // 1번 버튼 (양파)
             stepButtons[1].onClick.RemoveAllListeners();
-            stepButtons[1].onClick.AddListener(() => OnStepClick("OnionSlicing"));
+            stepButtons[1].onClick.AddListener(() => OnStepClick("onionslice"));
 
             // 2번 버튼 (캔따기)
             stepButtons[2].onClick.RemoveAllListeners();
